f93c739b921a0498a414e72d7aeec0e1e38f91b2
gauta <gauta@VENGEANCE.lan>
2024-02-05 21:54:47 -0600

    I finished 01 and started 02

--- src/_01_drawing/c_LogicalDot/c_LogicalDot.pde
+++ src/_01_drawing/c_LogicalDot/c_LogicalDot.pde
@@ -2,13 +2,14 @@
 void setup() {
   
 // 1. Set the size of the sketch. Make it 600 pixels square. 
-  
+  size(600,600);
 
 }
 
 void draw() {
   
 // 2. Draw an ellipse
+ellipse(300,300, 200,200);
 // Run the program to make sure it works before moving on.
 
 
@@ -17,11 +18,11 @@ void draw() {
 //    Remember to use the   fill()  command to set colors.
 
 if (mousePressed) {
- 
+ fill(#D8B2FF);
    //  put one color here 
   
 } else {
-  
+  fill(#B2C3FF);
    // put a different color here
   
 }  
--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -3,40 +3,41 @@ package _02_code_flow._1_robot_square;
  *    Copyright (c) The League of Amazing Programmers 2013-2020
  *    Level 0
  */
-
+// package is a library
 import org.jointheleague.graphical.robot.Robot;
 
 // This recipe draws a square using the Robot
 
 public class RobotSquare {
-	
+	// Class is a specific part of code that does something
     public static void main(String[] args){
- 
+ // main runs the code
     	// This code makes a new Robot
     	Robot rob = new Robot();
-
+// new robot saved into rob
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
-
+    	rob.penDown();
+    	// pendown & other functions are imported from a library
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed(<speed>);    **/
- 
+    	rob.setSpeed(10);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
-    	
+    	int count = 0;
 
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move(<distance>);    **/
-  
+    	rob.move(200);
 
 	// TURN.  Turn 90 degrees. Use this command:
     		  /**     rob.turn(<angle>);    **/
-
+    	rob.turn(90);
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
     		  /**     count += 1;    **/
-
+    	count +=1;
     	
     	// Run the program. Did the Robot draw a line and turn?
     	 
@@ -51,7 +52,11 @@ public class RobotSquare {
     	 *            INCREASE COUNT
     	 *      }
     	 **/
-    	
+    	while(count < 4) {
+    		rob.move(200);
+    		rob.turn(90);
+    		count+=1;
+    	}
     	
     	// Run the program again. Did your Robot draw a square?
 
